e61eb1a6791f4d73887da46694864c15d63a2f53
ryuukk <ryuukk.dev@gmail.com>
2024-08-03 17:14:37 +0200

    Improve library API

--- dcd_templates/src/dcd/server/dll.d
+++ dcd_templates/src/dcd/server/dll.d
@@ -1,5 +1,6 @@
 module dcd.server.dll;
 
+import std.experimental.logger: warning;
 import std.string: fromStringz;
 
 import core.runtime;
@@ -18,15 +19,22 @@ __gshared:
 
 ModuleCache cache;
 
-extern(C) export void dcd_init(string[] importPaths)
+extern(C) export void dcd_init()
 {
     rt_init();
-    cache.addImportPaths(importPaths);
 }
 
 extern(C) export void dcd_add_imports(string[] importPaths)
 {
-    cache.addImportPaths(importPaths);
+    string[] cloned;
+    cloned.length = importPaths.length;
+    for(int i = 0; i < importPaths.length; i++)
+    {
+        cloned[i] = importPaths[i].idup;
+
+        warning("adding import: ", cloned[i]);
+    }
+    cache.addImportPaths(cloned);
 }
 
 extern(C) export void dcd_clear()
@@ -128,6 +136,98 @@ extern(C) export DSymbolInfo[] dcd_document_symbols(const(char)* content)
         DSymbolInfo info;
         check(symbol, pos, &info);
         ret ~= info;
+    }
+
+    return ret;
+}
+
+extern(C) export DSymbolInfo[] dcd_document_symbols_sem(const(char)* content)
+{
+    import containers.ttree : TTree;
+    import containers.hashset;
+    import dcd.server.autocomplete.util;
+
+    import dparse.lexer;
+    import dparse.rollback_allocator;
+
+    import dsymbol.builtin.names;
+    import dsymbol.builtin.symbols;
+    import dsymbol.conversion;
+    import dsymbol.modulecache;
+    import dsymbol.scope_;
+    import dsymbol.string_interning;
+    import dsymbol.symbol;
+    import dsymbol.ufcs;
+    import dsymbol.utils;
+
+    import dcd.common.constants;
+    import dcd.common.messages;
+
+    DSymbolInfo[] ret;
+
+    AutocompleteRequest request;
+    request.fileName = "stdin";
+    request.cursorPosition = 0;
+    request.kind |= RequestKind.autocomplete;
+    request.sourceCode = cast(ubyte[]) fromStringz(content);
+
+    LexerConfig config;
+    config.fileName = "";
+    auto sc = StringCache(request.sourceCode.length.optimalBucketCount);
+    auto tokenArray = getTokensForParser(cast(ubyte[]) request.sourceCode, config, &sc);
+    RollbackAllocator rba;
+    auto pair = generateAutocompleteTrees(tokenArray, &rba, -1, cache);
+    scope(exit) pair.destroy();
+
+
+    size_t e;
+
+    void check(DSymbol* it)
+    {
+        //for (int i = 0; i < p; i++)
+        //fprintf(stderr, " ");
+        //fprintf(stderr, "loc: %ld k: %c sym: %.*s\n", it.location, cast(char) it.kind, it.name.length, it.name.ptr);
+
+        if (it.type != null)
+        {
+            //DSymbolInfo info;
+            //info.name = it.type.name;
+            //info.range[0] = it.type.location;
+
+            //if (it.type.location_end == 0)
+            //    info.range[1] = it.type.location + it.type.name.length;
+            //else
+            //    info.range[1] = it.type.location_end;
+            //info.kind = it.type.kind;
+            //ret ~= info;
+        }
+
+
+        DSymbolInfo info;
+        info.name = it.name;
+        info.range[0] = (it.location > e ? it.location - e : e - it.location );
+        
+        e = it.location;
+
+        if (it.location_end == 0)
+            info.range[1] = it.location + it.name.length;
+        else
+            info.range[1] = it.location_end;
+        info.kind = it.kind;
+        ret ~= info;
+
+        foreach(sym; it.opSlice())
+        {
+            if (sym.symbolFile != "stdin") continue;
+            
+            check(sym);
+       }
+    }
+
+    foreach (symbol; pair.scope_.symbols)
+    {
+        if (symbol.symbolFile != "stdin") continue;
+        check(symbol);
     }
 
     return ret;
